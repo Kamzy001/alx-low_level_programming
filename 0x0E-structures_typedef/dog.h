ce697b1f40f27bb6a2c0aa48b4bbd7fd26952a9f
root <Usman>
2023-03-25 17:06:27 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif11;rgb:0000/0000/0000
+#endif11; rgb:0000 / 0000 / 0000
