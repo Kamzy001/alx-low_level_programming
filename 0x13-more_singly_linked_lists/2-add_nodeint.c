71c9b00d576ce1a3532a60ebfd0793c3851ef571
kamzy001 <usmanaki@yahoo.com>
2023-04-04 04:30:24 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,24 +18,4 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new->next = *head;
 	*head = new;
 	return (new);
-} ts.h"
-
-/**
- * add_nodeint - function that adds a new node at the beginning of
- * @head: header pointer
- * @n: int data type
- * Return: the new address
- */
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-listint_t *new;
-
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->n = n;
-new->next = *head;
-*head = new;
-return (new);
-}~
+}
